963a03a7bf08111676e1d062ed8087ea40737544
Roman <kavoriya@gmail.com>
2023-10-15 20:16:43 +0500

    Build: project divs, contacts div, 'copied' pop-up

---
+++ script.js
@@ -0,0 +1,21 @@
+const copyContent = async(text) => {
+   try {
+      await navigator.clipboard.writeText(text);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+}
+
+
+function displayCopiedNotification() {
+   let notification = document.querySelector('.copied-notification');
+   if (notification.classList.contains('copied-notification_popped')) {
+      return;
+   }
+   notification.classList.add('copied-notification_popped');
+   setTimeout(() => {
+      notification.classList.remove('copied-notification_popped');
+   }, 2000);
+}
+
